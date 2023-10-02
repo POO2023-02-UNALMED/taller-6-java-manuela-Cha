1effb35db810fcb072e645e078c266c48a964f7d
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-10-01 23:23:47 -0500

    Update Automovil.java

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -1,1 +1,23 @@
+package vehiculos;
+public class Automovil extends Vehiculo{
 
+    private int puestos;
+    private static int numAutomoviles = 0; 
+    
+    public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos){
+        super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante );
+        this.puestos = puestos;
+        Automovil.numAutomoviles += 1;
+    }
+
+    public void setPuestos (int puestos){
+        this.puestos = puestos;
+    }
+    public int getPuestos (){
+        return puestos;
+    }
+
+    public static int cantAutomoviles(){
+        return Automovil.numAutomoviles;
+    }
+}
